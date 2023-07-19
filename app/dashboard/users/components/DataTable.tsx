ee3b6904751ed95ba8b039cf166af86b880f3ea4
dotslashf <fadhlurahman.i@gmail.com>
2023-07-19 11:41:28 +0800

    style: fix input search

--- app/dashboard/users/components/DataTable.tsx
+++ app/dashboard/users/components/DataTable.tsx
@@ -8,7 +8,7 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
-import { ClockIcon } from '@radix-ui/react-icons';
+import { ClockIcon, MagnifyingGlassIcon } from '@radix-ui/react-icons';
 import {
   ColumnDef,
   ColumnFiltersState,
@@ -23,6 +23,7 @@ import { formatDate } from '@/lib/utils';
 import { useState } from 'react';
 import { Input } from '@/components/ui/input';
 import { Badge } from '@/components/ui/badge';
+import CustomInput from '@/components/custom/Input';
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -97,16 +98,19 @@ export function DataTable<TData, TValue>({
   });
 
   return (
-    <div>
-      <div className="flex items-center py-4">
+    <div className="flex flex-col">
+      <div className="relative mb-4">
         <Input
-          placeholder="Filter emails..."
+          placeholder="Cari email..."
           value={(table.getColumn('email')?.getFilterValue() as string) ?? ''}
           onChange={(event) =>
             table.getColumn('email')?.setFilterValue(event.target.value)
           }
-          className="max-w-sm"
+          className="max-w-sm pl-11"
         />
+        <div className="absolute inset-y-0 left-0 z-20 flex items-center pl-4 pointer-events-none">
+          <MagnifyingGlassIcon className="w-4 h-4" />
+        </div>
       </div>
       <div className="border rounded-md">
         <Table>
