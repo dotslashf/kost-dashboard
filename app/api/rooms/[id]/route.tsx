be55fc53f33147d287bc95a137c3a5cbb8d4b129
dotslashf <fadhlurahman.i@gmail.com>
2023-07-22 22:55:13 +0800

    feat: add update room api

--- app/api/rooms/[id]/route.tsx
+++ app/api/rooms/[id]/route.tsx
@@ -35,3 +35,35 @@ export async function GET(req: any, { params }: { params: { id: string } }) {
     room,
   });
 }
+
+export async function PUT(req: any, { params }: { params: { id: string } }) {
+  const token = await getToken({ req });
+  if (token?.role !== 'ADMIN')
+    return new Response(JSON.stringify({ error: 'Unauthorized' }), {
+      status: 401,
+    });
+
+  const room = await prisma.room.findFirst({ where: { id: params.id } });
+  if (!room) return NextResponse.json({ error: 'Kamar tidak ditemukan' });
+
+  const body = (await req.json()) as {
+    name: string;
+    price: number;
+    details: string;
+  };
+
+  await prisma.room.update({
+    where: {
+      id: params.id,
+    },
+    data: {
+      name: body.name,
+      price: body.price,
+      details: body.details,
+    },
+  });
+
+  return NextResponse.json({
+    message: 'Kamar berhasil diupdate',
+  });
+}
